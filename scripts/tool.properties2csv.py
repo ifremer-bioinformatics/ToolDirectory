f5c1efd5a3cc864e0c37b10542157ba5207668f8
Alexandre Cormier <alexandre.cormier@ifremer.fr>
2020-04-27 15:21:00 +0000

    os.walk with dir level

--- scripts/tool.properties2csv.py
+++ scripts/tool.properties2csv.py
@@ -8,13 +8,75 @@ def getArgs():
     parser = argparse.ArgumentParser(description="")
     parser.add_argument('-p',dest="toolspath",type=str,default="../test/",help='')
     parser.add_argument('-o',dest="outpath",type=str,default="../test/",help='')
-    parser.add_argument('-s',dest="skipdir", default=[], type=lambda s: [str(item) for item in s.split(',')],help='Delimited list of tool folder(s) to ignore')
+    # parser.add_argument('-s',dest="skipdir", default=[], type=lambda s: [str(item) for item in s.split(',')],help='Delimited list of tool folder(s) to ignore')
 
     args = parser.parse_args()
 
     return args
 
-def parseProperties(directory, ignore):
+def walklevel(directory, depth = 2):
+    # If depth is negative, just walk
+    # Unsed in the project
+    if depth < 0:
+        for root, dirs, files in os.walk(directory):
+            yield root, dirs, files
+    # path.count works because is a file has a "/" it will show up in the list as a ":"
+    else:
+        path = directory.rstrip(os.path.sep)
+        num_sep = path.count(os.path.sep)
+        for root, dirs, files in os.walk(path):
+            yield root, dirs, files
+            num_sep_this = root.count(os.path.sep)
+            if num_sep + depth <= num_sep_this:
+                del dirs[:]
+
+# def parseProperties(directory, ignore):
+#     # Regexes to detect each category
+#     regexes = {
+#     'name':          'NAME=',
+#     'description':   'DESCRIPTION=',
+#     'version':       'VERSION=',
+#     'cmdline':       'CMDLINE=',
+#     'galaxy':        'GALAXY=',
+#     'documentation': 'URLDOC=',
+#     'operation':     'KEYWORDS=',
+#     'environment':   'CMD_INSTALL=',
+#     'topic':         'TOPIC='
+#     }
+#
+#     # Main dict with the result for each tool
+#     properties = {}
+#     # First, list level 1 dirs in order to not explore galaxy/ folder
+#     for dir in os.listdir(directory):
+#         # Ignore some dirs
+#         if not dir in ignore:
+#             # Second, list all files in all subdirectories
+#             # WARNING: beware of "latest" symlinks. os.walk() do not explore symlink folders by default so it's ok
+#             for root, dirs, files in os.walk(os.path.join(directory,dir)):
+#                 for file in files:
+#                     if file == "tool.properties":
+#                         # In case of error, get the full path to the file
+#                         # print(os.path.join(root, file))
+#                         # Open the tool.properties
+#                         tool_infos = open(os.path.join(root, file), 'r')
+#                         # Create a tmp dict
+#                         parsed_data = {}
+#                         parsed_data['path'] = root
+#                         for l in tool_infos:
+#                             # Use regex to ensure the correct recovery of each category
+#                             # In case of wrong category order
+#                             for k, v in regexes.items():
+#                                 if v in l:
+#                                     regex, value = re.split(r'=', l.rstrip('\n'))
+#                                     parsed_data[k] = value
+#                         # Create a uniq tool ID
+#                         toolID = parsed_data['name'] +'-'+ parsed_data['version']
+#                         # Transfert into the main dict
+#                         properties[toolID] = parsed_data
+#     # Return the dict for printing
+#     return(properties)
+
+def parseProperties(directories):
     # Regexes to detect each category
     regexes = {
     'name':          'NAME=',
@@ -30,35 +92,62 @@ def parseProperties(directory, ignore):
 
     # Main dict with the result for each tool
     properties = {}
-    # First, list level 1 dirs in order to not explore galaxy/ folder
-    for dir in os.listdir(directory):
-        # Ignore some dirs
-        if not dir in ignore:
-            # Second, list all files in all subdirectories
-            # WARNING: beware of "latest" symlinks. os.walk() do not explore symlink folders by default so it's ok
-            for root, dirs, files in os.walk(os.path.join(directory,dir)):
-                for file in files:
-                    if file == "tool.properties":
-                        # In case of error, get the full path to the file
-                        # print(os.path.join(root, file))
-                        # Open the tool.properties
-                        tool_infos = open(os.path.join(root, file), 'r')
-                        # Create a tmp dict
-                        parsed_data = {}
-                        parsed_data['path'] = root
-                        for l in tool_infos:
-                            # Use regex to ensure the correct recovery of each category
-                            # In case of wrong category order
-                            for k, v in regexes.items():
-                                if v in l:
-                                    regex, value = re.split(r'=', l.rstrip('\n'))
-                                    parsed_data[k] = value
-                        # Create a uniq tool ID
-                        toolID = parsed_data['name'] +'-'+ parsed_data['version']
-                        # Transfert into the main dict
-                        properties[toolID] = parsed_data
+    # Retrive all tool.properties
+    for root, dirs, files in directories:
+        if 'tool.properties' in files:
+            # print(os.path.join(root, 'tool.properties'))
+            tool_infos = open(os.path.join(root, 'tool.properties'), 'r')
+            parsed_data = {}
+            parsed_data['path'] = root
+            for l in tool_infos:
+                # Use regex to ensure the correct recovery of each category
+                # In case of wrong category order
+                for k, v in regexes.items():
+                    if v in l:
+                        regex, value = re.split(r'=', l.rstrip('\n'))
+                        parsed_data[k] = value
+            # Create a uniq tool ID
+            toolID = parsed_data['name'] +'-'+ parsed_data['version']
+            # Transfert into the main dict
+            properties[toolID] = parsed_data
     # Return the dict for printing
     return(properties)
+
+
+
+
+
+
+
+    # # First, list level 1 dirs in order to not explore galaxy/ folder
+    # for dir in os.listdir(directory):
+    #     # Ignore some dirs
+    #     if not dir in ignore:
+    #         # Second, list all files in all subdirectories
+    #         # WARNING: beware of "latest" symlinks. os.walk() do not explore symlink folders by default so it's ok
+    #         for root, dirs, files in os.walk(os.path.join(directory,dir)):
+    #             for file in files:
+    #                 if file == "tool.properties":
+    #                     # In case of error, get the full path to the file
+    #                     # print(os.path.join(root, file))
+    #                     # Open the tool.properties
+    #                     tool_infos = open(os.path.join(root, file), 'r')
+    #                     # Create a tmp dict
+    #                     parsed_data = {}
+    #                     parsed_data['path'] = root
+    #                     for l in tool_infos:
+    #                         # Use regex to ensure the correct recovery of each category
+    #                         # In case of wrong category order
+    #                         for k, v in regexes.items():
+    #                             if v in l:
+    #                                 regex, value = re.split(r'=', l.rstrip('\n'))
+    #                                 parsed_data[k] = value
+    #                     # Create a uniq tool ID
+    #                     toolID = parsed_data['name'] +'-'+ parsed_data['version']
+    #                     # Transfert into the main dict
+    #                     properties[toolID] = parsed_data
+    # # Return the dict for printing
+    # return(properties)
 
 def outputWriting(outpath, dictData):
     txt = open(os.path.join(outpath, 'Softwares.csv'), 'w')
@@ -83,9 +172,11 @@ def outputWriting(outpath, dictData):
     txt.close()
 
 def main(args):
-    # 1 - parse all tool.properties
-    parsedProperties = parseProperties(args.toolspath, args.skipdir)
-    # 2 - write csv file
+    # 1 - Digging tool directory and explore at 2 level
+    directories = walklevel(args.toolspath)
+    # 2 - parse all tool.properties
+    parsedProperties = parseProperties(directories)
+    # 3 - write csv file
     outputWriting(args.outpath ,parsedProperties)
 
 if __name__ == '__main__':
