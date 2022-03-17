6e84568c03c7609cfbb68062f464e6d51a98aaf6
Alexandre Cormier <alexandre.cormier@ifremer.fr>
2022-03-17 10:11:51 +0000

    change json structure for a single file per tool and add associated function

--- lib/core_tool.py
+++ lib/core_tool.py
@@ -1,12 +1,18 @@
 from __future__ import print_function
 import os
-import re
 import sys
 import json
-import shutil
 import requests
 import datetime
 from requests.exceptions import HTTPError
+
+environment = {
+    "b": "bash",
+    "c": "conda",
+    "d": "docker",
+    "s": "singularity",
+    "o": "other"
+}
 
 
 def walk_level(directory, depth=2):
@@ -36,6 +42,12 @@ def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
 
+def daytime():
+    now = datetime.datetime.now()
+    time = now.strftime("%d-%m-%Y")
+    return time
+
+
 def biotools_api_request(tool_id):
     try:
         response = requests.get('https://bio.tools/api/tool/' + tool_id + '/?format=json')
@@ -57,38 +69,32 @@ def biotools_api_request(tool_id):
         return response_json, code
 
 
-def check_module_path(args):
-    module_dir_path = os.path.join(args.path_modules, args.tool_name)
-    if not os.path.isdir(module_dir_path):
-        eprint(f"\033[0;31;47m ERROR:" + module_dir_path + " do not exist! \033[0m")
+def check_path(args):
+    """ Ensure that module file or compilation/conda folder exist depending on the cluster architecture
+    :param args:
+        - path: main path to module/tools directory
+        - name: tool name
+        - version: tool version
+    :return:
+    Nothing
+    """
+
+    tool_path = os.path.join(args.path, args.name)
+    if not os.path.isdir(tool_path):
+        eprint(f"\033[0;31;47m ERROR:" + tool_path + " do not exist! \033[0m")
         eprint(f"\033[0;31;47m ERROR: processus killed \033[0m")
         exit(1)
-    module_file_path = os.path.join(args.path_modules, args.tool_name, args.tool_version)
-    if not os.path.isfile(module_file_path):
-        eprint(f"\033[0;31;47m ERROR:" + module_file_path + " do not exist! \033[0m")
+    tool_version_path = os.path.join(args.path, args.name, args.version)
+    if not os.path.isfile(tool_version_path) and not os.path.isdir(tool_version_path):
+        eprint(f"\033[0;31;47m ERROR:" + tool_version_path + " do not exist! \033[0m")
         eprint(f"\033[0;31;47m ERROR: processus killed \033[0m")
         exit(1)
-
-
-def update_properties(args, properties):
-    eprint(f"\033[0;37;46m LOG: " + args.tool_name + " already exist. Adding new version number... \033[0m")
-    with open(properties) as json_data:
-        p = json.load(json_data)
-    p_check = p['VERSION'].split(',')
-    if args.tool_version not in p_check:
-        p['VERSION'] = p['VERSION'] + ',' + args.tool_version
-        with open(properties, 'w') as o:
-            json.dump(p, o, sort_keys=False, indent=2)
-        o.close()
-        eprint(f"\033[0;37;46m LOG: all done!\033[0m")
-    else:
-        eprint(f"\033[0;31;47m WARNING: tool version already declared. Skip.\033[0m")
 
 
 def create_properties(args, properties):
-    eprint(f"\033[0;37;46m LOG: Launch create for " + args.tool_name + " \033[0m")
+    eprint(f"\033[0;37;46m LOG: Launch create for " + args.name + " \033[0m")
     eprint(f"\033[0;37;46m LOG: Collect info. from bio.tools \033[0m")
-    response, code = biotools_api_request(args.tool_name)
+    response, code = biotools_api_request(args.name)
     eprint(f"\033[0;37;46m LOG: Load and parse JSON \033[0m")
     bio_json = json.loads(response)
     eprint(f"\033[0;37;46m LOG: Create properties.json \033[0m")
@@ -97,95 +103,79 @@ def create_properties(args, properties):
 
 
 def write_properties(args, biojson, properties):
-    now = datetime.datetime.now()
-    daytime = now.strftime("%d/%m/%Y")
-    prop = {
-        'NAME': biojson['biotoolsID'],
-        'DESCRIPTION': biojson['description'].split('\n')[0],
-        'VERSION': args.tool_version,
-        'URLDOC': biojson['homepage'],
-        'DATE_INSTALL': daytime,
-        'OWNER': args.owner
-    }
-    function = []
+    operation = []
     for i in biojson['function']:
-        operation = i['operation'][0]['term'].lower().replace(' ', '-')
-        function.append(operation)
-    prop['KEYWORDS'] = ','.join(list(set(function)))
+        operation.append(i['operation'][0]['term'].lower().replace(' ', '-'))
+    operations = ','.join(list(set(operation)))
     topic = []
     for i in biojson['topic']:
         topic.append(i['term'])
-    prop['TOPIC'] = ','.join(list(set(topic)))
-    if len(biojson['description'].split('\n')[0]) > 200:
-        eprint(f"\033[0;31;47m WARNING: tool description longer than 200 characters\033[0m")
+    topics = ','.join(list(set(topic)))
+    install_date = daytime()
+    if args.datetime:
+        install_date = args.datetime
+    prop = {
+        'name': biojson['biotoolsID'],
+        'description': biojson['description'].split('\n')[0],
+        'homepage': biojson['homepage'],
+        "operation": operations,
+        "topic": topics,
+        'version': {
+            args.version: {
+                "localInstallUser": args.installer,
+                "environment": environment[args.environment],
+                "localInstallDate": install_date,
+                "isCmdline": args.cmdline,
+                "isGalaxy": args.galaxy
+            }
+        }
+    }
+    if len(biojson['description'].split('\n')[0]) > 400:
+        eprint(f"\033[0;31;47m WARNING: tool description longer than 400 characters\033[0m")
     with open(properties, 'w') as out_json:
         json.dump(prop, out_json, sort_keys=False, indent=2)
     out_json.close()
 
 
-def collect_properties_to_upgrade(modifications):
-    updates = open(modifications, 'r')
-    update_data = {}
-    for l in updates:
-        tool, version, key = re.split(r'\t', l.rstrip('\n'))
-        key, val = key.split('=')
-        if tool not in update_data:
-            update_data[tool] = {version: {}}
-        if version not in update_data[tool]:
-            update_data[tool][version] = {}
-        update_data[tool][version][key] = val
-    return update_data
-
-
-def upgrade_properties(json_list, modifications, backup=False):
-    now = datetime.datetime.now()
-    daytime = now.strftime("%Y-%m-%d")
-    for j in json_list:
-        with open(j) as json_data:
-            p = json.load(json_data)
-        if "all" in modifications:
-            for k, v in modifications["all"]["all"].items():
-                p[k] = v
-        if p['NAME'] in modifications:
-            if p['VERSION'] in modifications[p['NAME']]:
-                for k, v in modifications[p['NAME']][p['VERSION']].items():
-                    p[k] = v
-        json_data.close()
-        if "all" in modifications:
-            if backup:
-                backup = j.replace('properties.json', 'properties.' + daytime)
-                shutil.copy2(j, backup)
-            with open(j, 'w') as out_json:
-                json.dump(p, out_json, sort_keys=False, indent=2)
-            out_json.close()
-        elif p['NAME'] in modifications and p['VERSION'] in modifications[p['NAME']]:
-            if backup:
-                backup = j.replace('properties.json', 'properties.' + daytime)
-                shutil.copy2(j, backup)
-            with open(j, 'w') as out_json:
-                json.dump(p, out_json, sort_keys=False, indent=2)
-            out_json.close()
-
-
-def get_properties_files(directories):
-    tool_prop_files = []
-    for root, dirs, files in directories:
-        if 'properties.json' in files:
-            abs_f = os.path.join(root, 'properties.json')
-            tool_prop_files.append(abs_f)
-    return tool_prop_files
+def add_version(args, properties):
+    eprint(f"\033[0;37;46m LOG: " + args.name + " already exist. Adding new version number... \033[0m")
+    with open(properties) as json_data:
+        p = json.load(json_data)
+    p_check = p['version'].keys()
+    install_date = daytime()
+    if args.datetime:
+        install_date = args.datetime
+    if args.version not in p_check:
+        p['version'][args.version] = {
+            "localInstallUser": args.installer,
+            "environment": environment[args.environment],
+            "localInstallDate": install_date,
+            "isCmdline": args.cmdline,
+            "isGalaxy": args.galaxy
+        }
+        with open(properties, 'w') as o:
+            json.dump(p, o, sort_keys=False, indent=2)
+        o.close()
+        eprint(f"\033[0;37;46m LOG: all done!\033[0m")
+    else:
+        eprint(f"\033[0;31;47m WARNING: tool version already declared. Skip.\033[0m")
 
 
 def kcsv_writing(csv_out, json_lst):
     txt = open(csv_out, 'w')
-    txt.write('Name,Version,Operation,Topic,Doc,Description,Date\n')
+    txt.write('Name,Version,Operation,Topic,Doc,Description\n')
 
     json_lst.sort(key=str.lower)
 
     for tool in json_lst:
         with open(tool) as json_data:
             p = json.load(json_data)
-        txt.write(
-            '"{0}","{1}","{2}","{3}","{4}","{5}","{6}"\n'.format(p['NAME'], p['VERSION'], p['KEYWORDS'], p['TOPIC'],
-                                                                 p['URLDOC'], p['DESCRIPTION'], p['DATE_INSTALL']))
+            versions = ','.join(sorted(p['version'].keys(), reverse=True))
+            txt.write('"{0}","{1}","{2}","{3}","{4}","{5}"\n'.format(p['name'],
+                                                                     versions,
+                                                                     p['operation'],
+                                                                     p['topic'],
+                                                                     p['homepage'],
+                                                                     p['description']))
+
     txt.close()
