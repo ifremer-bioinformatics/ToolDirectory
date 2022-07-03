049ac8ba3e06eae37e5635dab31839569f3a39e1
Alexandre Cormier <cormieralexandre@gmail.com>
2022-07-03 18:37:35 +0200

    Version 4 (#2)
    * rm old functions
    
    * centralized functions
    
    * direct function call
    
    * functions transfered to core_tool.py and call in tooldir
    
    * bug fix
    
    * fix doc and clean function names
    
    * rm upgrade + check tool path + rm duplicate fnct
    
    * change json structure for a single file per tool and add associated function
    
    * Add env. to each version
    
    * update file name
    
    * hotfix: rm test var
    
    * add ifremer specific function
    
    * add fields for the viewer
    
    * function for datarmor architecture
    
    * upgrade from v3 to v4 ifremer/ifb
    
    * bug fix and add worflow tag
    
    * rename core lib
    
    * datarmor properties
    
    * add installation status for each version
    
    * add installation status for each version
    
    * update documentation
    
    * create empty properties if tool not describe in Bio.tools

--- README.md
+++ README.md
@@ -21,14 +21,12 @@ Now, to create the CSV file requested by the Viewer in a fully automated way, To
 
 ToolDirectory provides tools to handle a catalogue of softwares relying on a particular way of installating softwares on your system (see below, Expected directory structure).
 
-This tool is a Python 3.x program. It also requires the following packages used to build the HTML report:
+ToolDirectory is a Python 3.x program. It also requires the following package:
 
-* Pandas (tested with release 0.21)
-* Jinja2 (tested with release 2.9.6)
-* Requests (tested with release 2.25.1)
+* Requests v2.25.1
 
 ```
-conda create -p tooldir-v3.0 -c jinja2 pandas requests
+conda create -p tooldir -c anaconda requests=2.25.1
 ```
 
 Web rendering relies on the open-source version of [Keshif](https://github.com/adilyalcin/Keshif) data visualisation. We provided [Katalog](https://gitlab.ifremer.fr/bioinfo/katalog), a lightweight version specifically designed for ToolDirectory and DataDirectory.
@@ -40,25 +38,25 @@ $ tooldir -h
 usage: tooldir <command> [<args>]
             The available commands are:
             create   Create tool properties
-            update   Update tool properties (add new key(s), change values)
-            upgrade  Upgrade tool properties to JSON (v2 -> v3)
+            status   Set installation status of tool/version
             kcsv     Create csv for visualisation
-            khtml    Create core webpage for visualisation
 ```
 
-## Prepare your Directory
-### 1 - Expected directory structure
+## Prepare installation
+### 1 - Expected structure
 
-Tool Directory expects a directory structure with the following constraints:
-
+ToolDirectory expects a directory structure with the following constraints:
 - \<install-dir>/\<tool>/\<version>/
+
+Or, with [modules](http://modules.sourceforge.net/) architecture:
+- \<install-dir>/\<tool>/<version-module>
 
 Here is an example:
 
 ```
-/appli/bioinfo
+/path/to/softwares
   ├── blast
-  │    ├── 2.2.31
+  │    ├── 2.2.31 #folder or module
   │    └── 2.6.0
   ├── plast
   │    └── 2.3.2
@@ -66,76 +64,12 @@ Here is an example:
   │    └── 4.3.0
   .../...
 ```
-### 2a - Declaring your softwares
 
-To declare your softwares to Tool Directory you just have to setup a propertie file, one per software. It contains the description of a single software and it has to follow these two constraints
-
-* file must be called ```properties.json```
-* file is located within home directory of a software
-
-Example:
-```
-{
-  "NAME": "PLAST",
-  "DESCRIPTION": "High Performance Parallel Local Alignment Search Tool",
-  "VERSION": "2.3.3",
-  "CMDLINE": "true",
-  "GALAXY": "true",
-  "URLDOC": "https://plast.inria.fr/user-guide/",
-  "KEYWORDS": "sequence-similarity-search",
-  "CMD_INSTALL": "shell",
-  "TOPIC": "Biological databases",
-  "DATE_INSTALL": "11/07/2018",
-  "OWNER": "galaxy"
-}
-```
-Keys explanation:
-
-```
-NAME: the name of the software
-DESCRIPTION: a short one line description of the software
-VERSION: the release tag of the software
-CMDLINE: software available on the command-line? (only use one of: true, false)
-GALAXY: software available on GALAXY Workflow platform? (only use one of: true, false)
-URLDOC: URL to the user manual
-KEYWORDS: one or several keywords to classify your software using EDAM Operation terms
-TOPIC: one or several keywords to classify your software using EDAM Topic terms
-CMD_INSTALL: the way a tool is installed. (one of: conda, docker, shell)
-OWNER: person who installed the software
-```
-
-A list of JSON files is available in `test/catalogue`.
-
-### 2b - Automatic creation of properties using Bio.tools API
-
-To be consistent between the descriptions of your softwares, we strongly recommend to use the Bio.tools database using ```tooldir create```
+### 2 - Creation of tool description using Bio.tools API
 
 ```bash
-tooldir create -n bowtie2 -v 2.3.5 -o username
+tooldir create -n <toolname> -v <version> -o <username>
 ```
-By default, it will search for the parameter file into  ```~/.tooldir.params``` which contains the structure of your install environment, as follow:
-
-```
-{
-    "install_dir_path": "/foo/bar/",
-    "conda_envs_path": "/foo/tools_env/",
-    "anaconda_dir_path": "/foo/anaconda/<versions>/",
-    "anaconda_profile_d": "/foo/anaconda/<versions>/etc/profile.d/conda.sh",
-    "topics":
-        [
-            "Bioimaging",
-            "Bioinformatics",
-            "Biological databases",
-            "Comparative genomics",
-            "Data visualisation",
-            "Statistics and probability",
-            "Structure analysis",
-            "Transcriptomics"
-        ]
-}
-```
-
-And you can indicate a restrictive list of allowed topics.
 
 ### 3 - Setup visualisation
 
@@ -146,12 +80,8 @@ You will need [Katalog](https://gitlab.ifremer.fr/bioinfo/katalog), a lightweigh
 git clone https://gitlab.ifremer.fr/bioinfo/katalog.git /foo/bar/www/tooldirectory
 ```
 
-Then, configure ToolDirectory and generate the software list:
+Then, generate the software list:
 ```bash
-# At the first utilization, generate a html webpage for visualization
-tooldir khtml -i webpage.json -p /path/to/www/katalog/
-
-# Generate csv file for Keshif visualization, at first time and for each update
 tooldir kcsv -p /path/to/softwares/ -o /foo/bar/www/tooldirectory/Softwares.csv
 ```
 
---
+++ lib/core.py
@@ -0,0 +1,239 @@
+from __future__ import print_function
+import os
+import sys
+import json
+import requests
+import datetime
+from requests.exceptions import HTTPError
+
+environment = {
+    "b": "bash",
+    "c": "conda",
+    "d": "docker",
+    "s": "singularity",
+    "o": "other"
+}
+
+
+def walk_level(directory, depth=2):
+    if depth < 0:
+        for root, dirs, files in os.walk(directory):
+            yield root, dirs, files
+    else:
+        path = directory.rstrip(os.path.sep)
+        num_sep = path.count(os.path.sep)
+        for root, dirs, files in os.walk(path):
+            yield root, dirs, files
+            num_sep_this = root.count(os.path.sep)
+            if num_sep + depth <= num_sep_this:
+                del dirs[:]
+
+
+def get_json(directories):
+    json_lst = []
+    for root, dirs, files in directories:
+        if 'properties.json' in files:
+            abs_f = os.path.join(root, 'properties.json')
+            json_lst.append(abs_f)
+    return json_lst
+
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+
+def daytime():
+    now = datetime.datetime.now()
+    time = now.strftime("%d-%m-%Y")
+    return time
+
+
+def biotools_api_request(args, properties):
+    try:
+        response = requests.get('https://bio.tools/api/tool/' + args.name + '/?format=json')
+        response.raise_for_status()
+    except HTTPError as http_error:
+        if response.status_code == 404:
+            # eprint(f"\033[0;31;47m WARNING: HTTP error occurred: {http_error} \033[0m")
+            eprint(f"\033[0;31;47m WARNING: " + args.name + " is not described in Bio.tools \033[0m")
+            eprint(f"\033[0;31;47m WARNING: writing empty properties file \033[0m")
+            eprint(f"\033[0;31;47m WARNING: please, fill it manual: {properties} \033[0m")
+            write_properties_default(args, properties)
+            exit(0)
+        else:
+            eprint(f"\033[0;31;47m ERROR: HTTP error occurred: {http_error} \033[0m")
+            exit(1)
+    except Exception as error:
+        eprint(f"\033[0;31;47m ERROR: Other error occurred: {error} \033[0m")
+        exit(1)
+    else:
+        response_json = response.text
+        code = response.status_code
+        return response_json, code
+
+
+def check_path(args):
+    tool_path = os.path.join(args.path, args.name)
+    if not os.path.isdir(tool_path):
+        eprint(f"\033[0;31;47m ERROR:" + tool_path + " do not exist! \033[0m")
+        eprint(f"\033[0;31;47m ERROR: processus killed \033[0m")
+        exit(1)
+    tool_version_path = os.path.join(args.path, args.name, args.version)
+    if not os.path.isfile(tool_version_path) and not os.path.isdir(tool_version_path):
+        eprint(f"\033[0;31;47m ERROR:" + tool_version_path + " do not exist! \033[0m")
+        eprint(f"\033[0;31;47m ERROR: processus killed \033[0m")
+        exit(1)
+
+
+def create_properties(args, properties):
+    eprint(f"\033[0;37;46m LOG: Launch create for " + args.name + " \033[0m")
+    eprint(f"\033[0;37;46m LOG: Collect info. from bio.tools \033[0m")
+    response, code = biotools_api_request(args, properties)
+    eprint(f"\033[0;37;46m LOG: Load and parse JSON \033[0m")
+    bio_json = json.loads(response)
+    eprint(f"\033[0;37;46m LOG: Create properties.json \033[0m")
+    write_properties(args, bio_json, properties)
+    eprint(f"\033[0;37;46m LOG: all done!\033[0m")
+
+
+def write_properties(args, biojson, properties):
+    operation = []
+    for i in biojson['function']:
+        operation.append(i['operation'][0]['term'].lower().replace(' ', '-'))
+    operations = ','.join(list(set(operation)))
+    topic = []
+    for i in biojson['topic']:
+        topic.append(i['term'])
+    topics = ','.join(list(set(topic)))
+    install_date = daytime()
+    if args.datetime:
+        install_date = args.datetime
+    prop = {
+        'name': biojson['biotoolsID'],
+        'description': biojson['description'].split('\n')[0],
+        'homepage': biojson['homepage'],
+        "operation": operations,
+        "topic": topics,
+        'version': {
+            args.version: {
+                "localInstallUser": args.installer,
+                "environment": environment[args.environment],
+                "localInstallDate": install_date,
+                "isCmdline": args.cmdline,
+                "isGalaxy": args.galaxy,
+                "isWorkflow": args.workflow,
+                "status": "active"
+            }
+        }
+    }
+    if len(biojson['description'].split('\n')[0]) > 400:
+        eprint(f"\033[0;31;47m WARNING: tool description longer than 400 characters\033[0m")
+    with open(properties, 'w') as out_json:
+        json.dump(prop, out_json, sort_keys=False, indent=2)
+    out_json.close()
+
+
+def write_properties_default(args, properties):
+    install_date = daytime()
+    if args.datetime:
+        install_date = args.datetime
+    prop = {
+        'name': args.name,
+        'description': '',
+        'homepage': '',
+        "operation": '',
+        "topic": '',
+        'version': {
+            args.version: {
+                "localInstallUser": args.installer,
+                "environment": environment[args.environment],
+                "localInstallDate": install_date,
+                "isCmdline": args.cmdline,
+                "isGalaxy": args.galaxy,
+                "isWorkflow": args.workflow,
+                "status": "active"
+            }
+        }
+    }
+    with open(properties, 'w') as out_json:
+        json.dump(prop, out_json, sort_keys=False, indent=2)
+    out_json.close()
+
+
+def add_version(args, properties):
+    eprint(f"\033[0;37;46m LOG: " + args.name + " already exist. Adding new version number... \033[0m")
+    with open(properties) as json_data:
+        p = json.load(json_data)
+    p_check = p['version'].keys()
+    install_date = daytime()
+    if args.datetime:
+        install_date = args.datetime
+    if args.version not in p_check:
+        p['version'][args.version] = {
+            "localInstallUser": args.installer,
+            "environment": environment[args.environment],
+            "localInstallDate": install_date,
+            "isCmdline": args.cmdline,
+            "isGalaxy": args.galaxy,
+            "isWorkflow": args.workflow,
+            "status": "active"
+        }
+        with open(properties, 'w') as o:
+            json.dump(p, o, sort_keys=False, indent=2)
+        o.close()
+        eprint(f"\033[0;37;46m LOG: all done!\033[0m")
+    else:
+        eprint(f"\033[0;31;47m WARNING: tool version already declared. Skip.\033[0m")
+
+
+def kcsv_writing(csv_out, json_lst):
+    txt = open(csv_out, 'w')
+    txt.write('Name,Version,Operation,Topic,Doc,Description,Environment,Galaxy,Workflow\n')
+
+    json_lst.sort(key=str.lower)
+
+    for tool in json_lst:
+        with open(tool) as json_data:
+            p = json.load(json_data)
+            ver_env = []
+            env = []
+            name = p['name']
+            operation = p['operation']
+            topic = p['topic']
+            homepage = p['homepage']
+            description = p['description']
+            for k in sorted(p['version'].keys(), reverse=True):
+                if p['version'][k]['status'] == 'active':
+                    ver_env.append(k + '-' + p['version'][k]['environment'])
+                    if p['version'][k]['environment'] not in env:
+                        env.append(p['version'][k]['environment'])
+            versions = ','.join(ver_env)
+            environments = ','.join(env)
+            isGalaxy = p['version'][k]['isGalaxy']
+            isWorkflow = p['version'][k]['isWorkflow']
+            # txt.write('"{0}","{1}","{2}","{3}","{4}","{5}","{6}","{7}","{8}"\n'.format(p['name'],
+            #                                                                versions,
+            #                                                                p['operation'],
+            #                                                                p['topic'],
+            #                                                                p['homepage'],
+            #                                                                p['description'],
+            #                                                                environments,
+            #                                                                isGalaxy,
+            #                                                                isWorkflow))
+            txt.write(f'"{name}","{versions}","{operation}","{topic}","{homepage}","{description}","{environments}","{isGalaxy}","{isWorkflow}"\n')
+    txt.close()
+
+
+def set_status(properties, versions, status):
+    with open(properties) as json_data:
+        p = json.load(json_data)
+    for version in versions:
+        try:
+            p['version'][version]['status'] = status
+        except KeyError:
+            eprint(f"\033[0;31;47m WARNING: invalid tool version: {versions}. Skip.\033[0m")
+            pass
+        with open(properties, 'w') as o:
+            json.dump(p, o, sort_keys=False, indent=2)
+        o.close()
+        eprint(f"\033[0;37;46m LOG: all done!\033[0m")
