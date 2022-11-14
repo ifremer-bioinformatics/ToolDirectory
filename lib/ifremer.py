bee175883c31becd686c653c7a3e39f25c2c73f6
Alexandre Cormier <cormieralexandre@gmail.com>
2022-11-14 16:10:35 +0100

    switch to loguru

--- lib/ifremer.py
+++ lib/ifremer.py
@@ -1,45 +1,40 @@
 import os
 import sys
-
-
-def eprint(*args, **kwargs):
-    print(*args, file=sys.stderr, **kwargs)
+from loguru import logger
 
 
 def make_tool_dir(params, tool_name, tool_version):
     install_dir_path = params['install_dir_path']
     path_tool = os.path.join(install_dir_path, tool_name, tool_version)
     if os.path.isdir(path_tool):
-        eprint(f"\033[0;31;47m WARNING:" + path_tool + " already exist! \033[0m")
-        eprint(f"\033[0;31;47m WARNING: file(s) inside will be overwritten \033[0m")
+        logger.warning(f"{path_tool} already exist")
+        logger.warning(f"File(s) inside will be overwritten")
     else:
-        eprint(f"\033[0;37;46m LOG: Create install folder at: " + path_tool + "\033[0m")
+        logger.info(f"Create install folder at: {path_tool}")
         os.makedirs(path_tool)
     return path_tool
 
 
 def conda_tool(params, tool_name, tool_version):
-    eprint(f"\033[0;37;46m LOG: Conda installation detected \033[0m")
+    logger.info(f"Conda installation")
     install_conda = params['conda_envs_path']
     install_dir = params['install_dir_path']
     path_tool = os.path.join(install_dir, tool_name, tool_version)
     env_tool = tool_name + '-' + tool_version
     path_env = os.path.join(install_conda, env_tool)
     if not os.path.isdir(path_env):
-        eprint(f"\033[0;31;47m ERROR:" + path_env + " do not exist! \033[0m")
-        eprint(f"\033[0;31;47m ERROR: Was conda environment has been created? \033[0m")
-        eprint(f"\033[0;31;47m ERROR: Processus killed \033[0m")
+        logger.error(f"{path_env} does not exist")
+        logger.error(f"Was conda environment has been created?")
         exit(1)
-
+    logger.info(f"Create env.sh and delenv.sh")
     env_sh = open(os.path.join(path_tool, 'env.sh'), 'w')
-    env_sh.write('#!/usr/bin/env bash\n')
-    env_sh.write('. ' + params['anaconda_profile_d'] + '\n')
-    env_sh.write('conda activate ' + path_env + '\n')
+    env_sh.write(f"#!/usr/bin/env bash\n")
+    env_sh.write(f". {params['anaconda_profile_d']}\n")
+    env_sh.write(f"conda activate {path_env}\n")
     env_sh.close()
     delenv_sh = open(os.path.join(path_tool, 'delenv.sh'), 'w')
-    delenv_sh.write('#!/usr/bin/env bash\n')
-    delenv_sh.write('conda deactivate\n')
-    delenv_sh.write('PATH=$(echo "$PATH" | sed -e "s@' + params['anaconda_dir_path'] + '.*/condabin:@@g")\n')
-    delenv_sh.write('unset CONDA_EXE CONDA_PYTHON_EXE\n')
+    delenv_sh.write(f"#!/usr/bin/env bash\n")
+    delenv_sh.write(f"conda deactivate\n")
+    delenv_sh.write(f'PATH=$(echo "$PATH" | sed -e "s@ {params["anaconda_dir_path"]}.*/condabin:@@g")\n')
+    delenv_sh.write(f"unset CONDA_EXE CONDA_PYTHON_EXE\n")
     delenv_sh.close()
-    eprint(f"\033[0;37;46m LOG: Create env.sh and delenv.sh \033[0m")
